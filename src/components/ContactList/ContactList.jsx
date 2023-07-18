4492ef66bb639349bc16a6f373a792339bcaa4e1
Aleksandra Aleksińska <aleksandra.aleksinska@gmail.com>
2023-07-18 20:12:11 +0200

    css button little update

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,13 +2,14 @@ import React, { Fragment } from 'react';
 import { useSelector } from 'react-redux';
 import { selectFilteredContacts } from 'redux/selectors';
 import ContactListItem from './ContactListItem';
+import css from './ContactList.module.css'
 
 const ContactList = () => {
   
  
   return (
     <Fragment>
-    <ul>            
+    <ul className={css.contactList}>            
       <ContactListItem 
       contacts={useSelector(selectFilteredContacts)}
       />     
